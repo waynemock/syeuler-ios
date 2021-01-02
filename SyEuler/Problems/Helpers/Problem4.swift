b145670f36cd64bd0d88ac51575044f0f157daeb
Wayne Mock <wayne.mock@icloud.com>
2021-01-01 19:25:24 -0600

    Updated the placeholder

--- SyEuler/Problems/Helpers/Problem4.swift
+++ SyEuler/Problems/Helpers/Problem4.swift
@@ -13,10 +13,11 @@ class Problem4: Problem {
 	public override var title: String { return "Largest palindrome product" }
 	public override var summary: String { return "Find the largest palindrome made from the product of two 3-digit numbers." }
 	public override var defaultInput: String { return "3" }
-	public override var inputPlaceholder: String { return "Number of digits" }
+	public override var inputPlaceholder: String { return "Enter the number of digits" }
 
-	public override var state: State { return .inProgress }
+	public override var state: State { return .completed }
 	public override var startedAt: Date? { return Problem.dateFormatter.date(from: "Jan 1, 2021") }
+	public override var completedAt: Date? { return startedAt }
 
 	public override func getOp(inputs: [String], completion: @escaping ProblemOpCompletion) -> ProblemOp {
 		return Problem4Op(inputs: inputs, completion: completion)
