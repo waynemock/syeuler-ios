5ac805d12400eefe785356274e19bd3b0a127f3e
Wayne Mock <wayne.mock@icloud.com>
2021-01-14 09:51:53 -0600

    Completed problem 11

--- SyEuler/Supporting/Playground.playground/Contents.swift
+++ SyEuler/Supporting/Playground.playground/Contents.swift
@@ -1,63 +1,78 @@
 // Here's my solution in Swift. Just copy it into a Playground and press play.
-// Finished in 10 seconds on my iPhone 12 Pro
+// Finished in 4 ms on my iPhone 12 Pro
 
 import Foundation
 
 /**
-Returns the sum of all primes below `target`.
+Returns the largest product of `target` consecutive digits in a 20x20 grid.
 
-Uses simplified Wheel Factorization to determine the `target`th prime.
-
-Reference:
-https://en.m.wikipedia.org/wiki/Wheel_factorization
-
-- Parameter target: The Nth number to generate.
-- Returns: The sum, or `nil` if cancelled before completion.
+- Parameter target: The number of digits to consider.
+- Returns: The product, or `nil` if `target` is out of range..
 */
 func compute(target: Int) -> Int? {
-	guard target > 0 else { return nil }
-	/// Initial set of primes to kick off the wheel factorization
-	var primes = [2,3,5]
-	var k = 7, i = 0
-	while k < target {
-		if isPrime(k, primes) {
-			primes.append(k)
-		}
-		k += inc[i]
-		i = (i + 1) % inc.count
-	}
-	return primes.reduce(0, { $0 + $1 })
-}
-
-/**
-Returns `true` if `value` is prime.
-
-Uses the existing `primes` array to determine if `value` is prime.
-If it is not a multiple of any existing prime, then it is prime.
-
-- Parameter value: The value to test for prime.
-- Parameter primes: The current array of primes.
-- Returns: `true` if `value` is prime, otherwise `false`.
-*/
-func isPrime(_ value: Int, _ primes: [Int]) -> Bool {
-	let sqRoot = Int(sqrt(Double(value)))
-	guard value % sqRoot != 0 else { return false }
-	for index in 0..<primes.count {
-		let prime = primes[index]
-		if value % prime == 0 {
-			/// Not prime if `value` is a multiple of a prime
-			return false
-		}
-		if sqRoot < prime {
-			/// Stop looking if the primes are larger than the square root
-			break
+	guard target > 0 && target <= 10 else { return nil }
+	var max = 0
+	let stop = width - target
+	for y in 0..<width {
+		for x in 0..<width {
+			var horiz = 1, vert = 1, left = 1, right = 1
+			for r in 0..<target {
+				if x <= stop {
+					horiz *= cell(x + r, y)
+				}
+				if y <= stop {
+					vert *= cell(x, y + r)
+				}
+				if x >= target - 1 && y <= stop {
+					left *= cell(x - r, y + r)
+				}
+				if x <= stop && y <= stop {
+					right *= cell(x + r, y + r)
+				}
+			}
+			if horiz > max {
+				max = horiz
+			}
+			if vert > max {
+				max = vert
+			}
+			if left > max {
+				max = left
+			}
+			if right > max {
+				max = right
+			}
 		}
 	}
-	return true
+	return max
 }
 
-/// Increments for finding the next prime.
-let inc = [4, 2, 4, 2, 4, 6, 2, 6]
+func cell(_ x: Int, _ y: Int) -> Int {
+	return data[(y * width) + x]
+}
 
-compute(target: 10)
-compute(target: 2000000)
+let width = 20
+let data = [
+	08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08,
+	49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00,
+	81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65,
+	52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91,
+	22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80,
+	24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50,
+	32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70,
+	67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21,
+	24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72,
+	21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95,
+	78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92,
+	16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57,
+	86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58,
+	19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40,
+	04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66,
+	88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69,
+	04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36,
+	20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16,
+	20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54,
+	01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48,
+]
+
+compute(target: 4)
---
+++ SyEulerTests/Problem11Tests.swift
@@ -0,0 +1,38 @@
+//
+//  Problem11Tests.swift
+//  SyEulerTests
+//
+//  Created by Wayne Mock on 1/14/21.
+//  Copyright © 2021 Syzygy Softwerks LLC. All rights reserved.
+//
+
+import XCTest
+@testable import SyEuler
+
+class Problem11Tests: XCTestCase {
+
+	let problem = Problem11Op(inputs: []) { results in }
+
+	func testTargetOutOfRange() throws {
+		[0, 11].forEach { n in
+			let answer = problem.compute(target: 0)
+			XCTAssertEqual(answer?.value, nil)
+			XCTAssertEqual(answer?.details, nil)
+			XCTAssertEqual(answer?.error, "Must be > 0 and <= 10")
+		}
+	}
+
+	func testAnswers() throws {
+		[(1, 99), (2, 9603), (3, 811502), (4, 70600674)].forEach { (n, value) in
+			let answer = problem.compute(target: n)
+			XCTAssertEqual(answer?.value, value)
+		}
+	}
+
+	func testResults() throws {
+		for n in 1...10 {
+			let answer = problem.compute(target: n)
+			XCTAssertEqual(answer?.value, problem.values.reduce(1, { $0 * $1 }))
+		}
+	}
+}
